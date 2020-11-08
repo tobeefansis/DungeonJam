ad51b04329c3161abac8e05cf6259062d63b0c06
tobeefansis <tftsis859@gmail.com>
2020-11-08 14:04:14 +0300

    update

--- Assets/Scripts/GraveSpawner.cs
+++ Assets/Scripts/GraveSpawner.cs
@@ -5,15 +5,16 @@ using System.Collections.Generic;
 public class GraveSpawner : MonoBehaviour
 {
     [SerializeField] List<GameObject> GravePrefubs;
+    [SerializeField] List<GameObject> GravePrefubs2;
 
     void Start()
     {
-        if (PlayerSettings.InstanceExists)
+        print("asdasdasdasd2");
+        foreach (var item in PlayerSettings.Instance.DeadPositions)
         {
-            foreach (var item in PlayerSettings.Instance.DeadPositions)
-            {
-                Instantiate(GravePrefubs[Random.Range(0, GravePrefubs.Count)], item, Quaternion.identity);
-            }
+            print("asdasdasdasd");
+            GravePrefubs2.Add(Instantiate(GravePrefubs[Random.Range(0, GravePrefubs.Count)], item, Quaternion.identity));
         }
+        print("asdasdasdasd4324");
     }
 }
---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour
+{
+    private float startPos, length;
+    public GameObject camera;
+    public float paralaxEffect;
+
+    void Start()
+    {
+        startPos = transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.y;
+    }
+
+    void Update()
+    {
+        float temp = camera.transform.position.y * (1 - paralaxEffect);
+        float dist = camera.transform.position.y * paralaxEffect;
+
+        // двигаем фон с поправкой на paralaxEffect
+        transform.position = new Vector3(transform.position.x, startPos + dist, transform.position.z);
+
+        // если камера перескочила спрайт, то меняем startPos
+        if (temp > startPos + length)
+            startPos += length;
+        else if (temp < startPos - length)
+            startPos -= length;
+    }
+}
